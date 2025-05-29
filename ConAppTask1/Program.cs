f99ffda63014635aa5e3d530b58e56002d3024a1
Kostya Skorohod <skorohodsokil@gmail.com>
2025-05-29 03:35:44 +0300

    first commit

---
+++ ConAppTask1/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace ConAppTask1;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Виберіть спосіб введення даних:");
+        Console.WriteLine("1. З файлу");
+        Console.WriteLine("2. З клавіатури");
+        Console.WriteLine("3. Випадкові числа");
+        Console.Write("Ваш вибір (1-3): ");
+        string choice = Console.ReadLine();
+
+        int[] array;
+        int size = 0;
+
+        if (choice == "1")
+        {
+            Console.Write("Введіть шлях до файлу: ");
+            string path = Console.ReadLine();
+            string[] lines = File.ReadAllLines(path);
+            size = lines.Length;
+            array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = Convert.ToInt32(lines[i]);
+            }
+        }
+        else if (choice == "2")
+        {
+            Console.Write("Введіть розмір масиву: ");
+            size = Convert.ToInt32(Console.ReadLine());
+            array = new int[size];
+            Console.WriteLine("Введіть елементи масиву:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Елемент " + (i + 1) + ": ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        else if (choice == "3")
+        {
+            Console.Write("Введіть розмір масиву: ");
+            size = Convert.ToInt32(Console.ReadLine());
+            array = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rnd.Next(-50, 51); 
+            }
+        }
+        else
+        {
+            Console.WriteLine("Невірний вибір. Завершення програми.");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.WriteLine("\nПочатковий масив:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write("{0,5}", array[i]);
+        }
+
+        
+        int max1 = array[0];
+        int max2 = array[0];
+        int max3 = array[0];
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > max1)
+            {
+                max3 = max2;
+                max2 = max1;
+                max1 = array[i];
+            }
+            else if (array[i] > max2)
+            {
+                max3 = max2;
+                max2 = array[i];
+            }
+            else if (array[i] > max3)
+            {
+                max3 = array[i];
+            }
+        }
+        int sum = max1 + max2 + max3;
+        Console.WriteLine("\n\nСума трьох найбільших елементів: " + sum);
+
+        
+        for (int i = 0; i < array.Length; i++)
+        {
+            if ((i + 1) % 10 == 0) 
+            {
+                array[i] = array[i] + sum;
+            }
+            else if ((i + 1) % 5 == 0) 
+            {
+                array[i] = array[i] - sum;
+            }
+        }
+
+        Console.WriteLine("\nМодифікований масив:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write("{0,5}", array[i]);
+        }
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
---
+++ ConAppTask2/Program.cs
@@ -0,0 +1,153 @@
+﻿namespace ConAppTask2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Виберіть спосіб введення даних:");
+        Console.WriteLine("1. З файлу");
+        Console.WriteLine("2. З клавіатури");
+        Console.WriteLine("3. Випадкові числа");
+        Console.Write("Ваш вибір (1-3): ");
+        string choice = Console.ReadLine();
+
+        int m, n;
+        int[,] matrix;
+
+        if (choice == "1")
+        {
+            Console.Write("Введіть шлях до файлу: ");
+            string path = Console.ReadLine();
+            string[] lines = File.ReadAllLines(path);
+            m = Convert.ToInt32(lines[0]);
+            n = Convert.ToInt32(lines[1]);
+            matrix = new int[m, n];
+            int index = 2;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(lines[index]);
+                    index++;
+                }
+            }
+        }
+        else if (choice == "2")
+        {
+            Console.Write("Введіть кількість рядків: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть кількість стовпців: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            matrix = new int[m, n];
+            Console.WriteLine("Введіть елементи масиву:");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Елемент [" + i + "," + j + "]: ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+        else if (choice == "3")
+        {
+            Console.Write("Введіть кількість рядків: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть кількість стовпців: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            matrix = new int[m, n];
+            Random rnd = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rnd.Next(-50, 51);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Невірний вибір. Завершення програми.");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.WriteLine("\nПочатковий масив:");
+        Console.Write("    ");
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write("{0,5}", j);
+        }
+        Console.WriteLine("\n   ------------------");
+        for (int i = 0; i < m; i++)
+        {
+            Console.Write("{0,2}", i);
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("{0,5}", matrix[i, j]);
+            }
+            Console.WriteLine();
+        }
+
+        double minRs = 999999; 
+        int minRsColumn = -1;
+        for (int j = 0; j < n; j++)
+        {
+            int hasNegative = 0;
+            double sumPositive = 0;
+            int countPositive = 0;
+            for (int i = 0; i < m; i++)
+            {
+                if (matrix[i, j] < 0)
+                {
+                    hasNegative = 1;
+                }
+                if (matrix[i, j] > 0)
+                {
+                    sumPositive = sumPositive + matrix[i, j];
+                    countPositive++;
+                }
+            }
+            if (hasNegative == 1 && countPositive > 0)
+            {
+                double rs = sumPositive / countPositive;
+                if (rs < minRs)
+                {
+                    minRs = rs;
+                    minRsColumn = j;
+                }
+            }
+        }
+
+        if (minRsColumn == -1)
+        {
+            Console.WriteLine("\nНемає стовпців з від'ємними та позитивними елементами.");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.WriteLine("\nНайменше Rs = " + minRs + " у стовпці " + minRsColumn);
+
+        for (int i = 0; i < m; i++)
+        {
+            matrix[i, minRsColumn] = 0;
+        }
+
+        Console.WriteLine("\nМодифікований масив:");
+        Console.Write("    ");
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write("{0,5}", j);
+        }
+        Console.WriteLine("\n   ------------------");
+        for (int i = 0; i < m; i++)
+        {
+            Console.Write("{0,2}", i);
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write("{0,5}", matrix[i, j]);
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
